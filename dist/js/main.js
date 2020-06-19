0fbee9ecfeb04ea621e22632cbb10b832af48748
Gagik <gagik.n@gmail.com>
2020-06-20 01:04:23 +0200

    nav menu overlay, transition and styling nav items

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,11 +4,11 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-items');
+const navItems = document.querySelectorAll('.nav-item');
 
 //  stat of menu
 
-let showMenu = true;
+let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
 
@@ -17,6 +17,7 @@ function toggleMenu() {
 		menuBtn.classList.add('close');
 		menu.classList.add('show');
 		menuNav.classList.add('show');
+		menuBranding.classList.add('show');
 		navItems.forEach((item) => item.classList.add('show'));
 		// changing menu state
 		showMenu = true;
@@ -24,6 +25,8 @@ function toggleMenu() {
 		menuBtn.classList.remove('close');
 		menu.classList.remove('show');
 		menuNav.classList.remove('show');
+		menuBranding.classList.remove('show');
+
 		navItems.forEach((item) => item.classList.remove('show'));
 		showMenu = false;
 	}
