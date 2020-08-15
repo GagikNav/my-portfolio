5a3d8a8fd5fed58e2354e5ae40e75d8e362587c5
gagik <gagik.n@gmail.com>
2020-08-16 00:56:13 +0200

    Add modal with primary styles, about page bio styling chang

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,7 @@
 // Selecting Dom
 
-'strict';
+'use strict';
+
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
@@ -54,3 +55,31 @@ switch (title) {
 		break;
 	default:
 }
+
+//  modal section
+//
+// Get the modal
+const modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+const btn = document.getElementById('myBtn');
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName('modal-close')[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+	modal.style.display = 'block';
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+	modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = 'none';
+	}
+};
